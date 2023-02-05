87d850bfd1d1718bcfed49339ca5cd3c53b30168
Mohd Umair Ashraf <umair.ashrafyo@gmail.com>
2023-02-05 18:03:29 +0530

    first commit

---
+++ carzone_environment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\umair\Downloads\Python Django\CarZoneProject\carzone_environment\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
